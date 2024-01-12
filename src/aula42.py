cb885c0aaf67afe9b0ddf630dc0e53a2bca12a8d
Alex Antonio Silva <alexs@dbserver.com.br>
2024-01-12 11:27:46 -0300

    Iterando strings com While

---
+++ src/aula42.py
@@ -0,0 +1,6 @@
+frase = 'O Python é uma linguagem de programação '\
+        'multiparadigma. '\
+        'Python foi criado por Guido Van Rossum.'.upper()
+
+print(frase)
+print(frase.count('Python'))
